76536d5af8594a99be7e559132774fc782e0b7b6
Kaspar Streiff <streikas@students.zhaw.ch>
2024-04-28 16:07:03 +0200

    Changed naming MainWindowController

---
+++ app/src/main/java/ch/zhaw/it/pm2/receiptsplitter/controller/MainWindowController.java
@@ -0,0 +1,18 @@
+package ch.zhaw.it.pm2.receiptsplitter.controller;
+
+public class MainWindowController implements CanNavigate, CanReset {
+    @Override
+    public void confirm() {
+
+    }
+
+    @Override
+    public void back() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
