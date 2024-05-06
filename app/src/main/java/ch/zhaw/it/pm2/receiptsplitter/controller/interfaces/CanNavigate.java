cbe880081f1ac76f45b25521db1b65536efb6ae2
Suhejl Asani <asanisuh@students.zhaw.ch>
2024-05-06 22:28:06 +0200

    Code refactoring

--- app/src/main/java/ch/zhaw/it/pm2/receiptsplitter/controller/interfaces/CanNavigate.java
+++ app/src/main/java/ch/zhaw/it/pm2/receiptsplitter/controller/interfaces/CanNavigate.java
@@ -2,7 +2,13 @@ package ch.zhaw.it.pm2.receiptsplitter.controller.interfaces;
 
 public interface CanNavigate {
 
+    /**
+     * Switches to the next scene.
+     */
     void confirm();
 
+    /**
+     * Switches to the previous scene.
+     */
     void back();
 }
